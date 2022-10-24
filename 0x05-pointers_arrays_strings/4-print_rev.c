bfccb6ef23d0d384230c75fa7b689bf1317ed2eb
Haniemar <haniemar19@gmail.com>
2022-10-24 15:18:46 -0700

    4-8

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - function that prints string in reverse
+ *
+ * @s: a string
+ *
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+        int i = 0;
+
+        while (s[i])
+        {
+                i++;
+        }
+        while (i--)
+        {
+                _putchar(s[i]);
+        }
+        _putchar('\n');
+}
